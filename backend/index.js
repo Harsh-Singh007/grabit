4c6355ef9ae68eb37f4216a3f67948870809db48
Harsh Singh <harsh.65004@gmail.com>
2026-01-28 20:28:19 +0530

    Update CORS for production

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,11 @@ import { connectCloudinary } from "./config/cloudinary.js";
 const app = express();
 
 await connectCloudinary();
-// allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL
+].filter(Boolean);
+
 //middlewares
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(cookieParser());
